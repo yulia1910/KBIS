a6c9fb556fb284454a08d2169fdae387296f2791
Igor <okigor@gmail.com>
2020-12-02 10:37:21 +0200

    README

--- src/store/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './todo.actions';
-export * from './todo.effrects';
-export * from './todo.reducer';
-export * from './todo.selectors';
--- src/store/todo.actions.ts
+++
@@ -1,8 +0,0 @@
-import { TodoItem } from './../app/model/todo-item.model';
-import { createAction, props } from '@ngrx/store';
-
-export const loadTodoItemsAction = createAction('[Todo Items] Load all items');
-export const loadTodoItemsSuccessAction = createAction('[Todo Items] Load all success', props<{ data: TodoItem[] }>());
-export const loadTodoItemsFailedAction = createAction('[Todo Items] Load all failed', props<{ error: any }>());
-export const saveItemAction = createAction('[Todo Items] save item', props<{ item: TodoItem }>());
-export const saveItemSuccessAction = createAction('[Todo Items] save success', props<{ item: TodoItem }>());
